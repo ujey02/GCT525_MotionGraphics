f8bccbbc5acd84b9c4e0d689c98c1a275e19ab2d
J Yoo <ujey02@gmail.com>
2019-03-28 19:53:51 +0900

    Add files via upload

---
+++ BezierCurve.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierCurve : MonoBehaviour
+{
+    public GameObject[] Point_Objects;
+    public GameObject Airplane;
+    public LineRenderer lineRenderer;
+
+    // # of points = 100
+    public Vector3[] BezierPoints;
+
+    Vector3[] ControlPoints = new Vector3[4];
+
+    [Range(0.0f, 1.0f)]
+    public float parameter_t; // parameter
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetControlPoints();
+        DrawBezierCurve();
+
+        //////////////////////////
+        /// #14                ///
+        /// Implement Update() ///
+        //////////////////////////
+
+        // Move the airplane to the point of Bezier Curve at parameter_t (use Airplane.transform.position)
+
+        // Compute 3 orthogonal vector3 of frenet frame
+
+        // Visualize the axes of frenet frame (use Debug.DrawLine())
+
+        // Generate rotation matrix with basis vectors of franet frame and convert it to a quaternion (use Matrix4x4, RotToQuat())
+
+        // Set airplane's rotation (use Airplane.transform.rotation)
+
+        Airplane.transform.position = ComputeBezierPoint(parameter_t);
+        Vector3 curPos = Airplane.transform.position;
+        Vector3 first_derivative = ComputeBezierTangent(parameter_t);
+        Vector3 second_derivative = ComputeBezier2ndDerivative(parameter_t);
+
+        Vector3 B = ComputeFrenetNormal(first_derivative, second_derivative);
+        Debug.DrawLine(curPos, curPos+first_derivative*20, Color.blue);
+        Debug.DrawLine(curPos, curPos+second_derivative*20, Color.red);
+        Debug.DrawLine(curPos, curPos+B*20, Color.green);
+
+        Matrix4x4 rotMat = new Matrix4x4();
+        rotMat.SetColumn(0, new Vector4(second_derivative.x, second_derivative.y,second_derivative.z,0));
+        rotMat.SetColumn(1, new Vector4(B.x, B.y, B.z, 0));
+        rotMat.SetColumn(2, new Vector4(first_derivative.x, first_derivative.y, first_derivative.z, 0));
+        rotMat.SetColumn(3, new Vector4(curPos.x,curPos.y,curPos.z,1));
+        Airplane.transform.rotation = RotToQuat(rotMat);
+    }
+
+    void GetControlPoints() {
+        for (int i = 0; i < 4; i++)
+        {
+            ControlPoints[i].x = Point_Objects[i].transform.position.x;
+            ControlPoints[i].y = Point_Objects[i].transform.position.y;
+            ControlPoints[i].z = Point_Objects[i].transform.position.z;
+        }
+    }
+
+    Vector3 ComputeBezierPoint(float t)
+    {
+        Vector3 b_t = Vector3.zero;
+
+        ///////////////////////////////////////////
+        /// #8                                  ///
+        /// find the point on Bezier Curve at t ///
+        ///////////////////////////////////////////
+
+        b_t.x = ControlPoints[0].x * Mathf.Pow(1 - t, 3) + ControlPoints[1].x * Mathf.Pow(1 - t, 2) * 3 * t + ControlPoints[2].x * (1 - t) * 3 * Mathf.Pow(t, 2) + ControlPoints[3].x * Mathf.Pow(t, 3);
+        b_t.y = ControlPoints[0].y * Mathf.Pow(1 - t, 3) + ControlPoints[1].y * Mathf.Pow(1 - t, 2) * 3 * t + ControlPoints[2].y * (1 - t) * 3 * Mathf.Pow(t, 2) + ControlPoints[3].y * Mathf.Pow(t, 3);
+        b_t.z = ControlPoints[0].z * Mathf.Pow(1 - t, 3) + ControlPoints[1].z * Mathf.Pow(1 - t, 2) * 3 * t + ControlPoints[2].z * (1 - t) * 3 * Mathf.Pow(t, 2) + ControlPoints[3].z * Mathf.Pow(t, 3);
+
+
+        return b_t;
+    }
+
+    void DrawBezierCurve() {
+
+        /////////////////////////////////////////////////////////
+        /// #9                                                ///
+        /// Draw a Bezier curve with points in BezierPoints[] ///
+        /// (use ComputeBezierPoint(), LineRenderer)          ///
+        /////////////////////////////////////////////////////////
+        for (int i = 0; i < 100; i++)
+        {
+            BezierPoints[i] = ComputeBezierPoint(i / (float)100);
+            lineRenderer.positionCount = 100;
+            lineRenderer.SetPosition(i, BezierPoints[i]);
+        }
+
+        
+    }
+
+    Vector3 ComputeBezierTangent(float t) {
+        Vector3 tangent = Vector3.zero;
+
+        /////////////////////////////////////////////////////
+        /// #10                                           ///
+        /// compute tangent vector of a Bezier Curve at t ///
+        /////////////////////////////////////////////////////
+        
+        float L = 3;
+
+        //float[] B_poly1 = new float[] { 1 - t, t };
+        float[] B_poly2 = new float[] { Mathf.Pow(1 - t, 2), 2 * (1 - t) * t, Mathf.Pow(t, 2) };
+        //float[] B_poly3 = new float[] { Mathf.Pow(1 - t, 3), 3 * Mathf.Pow(1 - t, 2) * t, 3 * (1 - t) * Mathf.Pow(t, 2), Mathf.Pow(t, 3) };
+        Vector3 sum = new Vector3(0,0,0);
+        for (int i =0; i<3; i++)
+        {
+            Vector3 deltaP = ControlPoints[i+1] - ControlPoints[i];
+
+            sum += B_poly2[i] * deltaP;
+        }
+
+        tangent = L * sum;
+
+
+        return tangent.normalized;
+    }
+
+    Vector3 ComputeBezier2ndDerivative(float t) {
+        Vector3 second_d = Vector3.zero;
+
+        /////////////////////////////////////////////////////
+        /// #11                                           ///
+        /// compute 2nd derivative of a Bezier Curve at t ///
+        /////////////////////////////////////////////////////
+        float[] B_poly1 = new float[] { 1 - t, t };
+
+        float L = 3;
+
+        Vector3 sum = new Vector3(0, 0, 0);
+        for (int i = 0; i < 2; i++)
+        {
+
+            Vector3 delta2P = ControlPoints[i + 2] - 2 * ControlPoints[i + 1] + ControlPoints[i];
+            sum += B_poly1[i] * delta2P;
+
+        }
+
+        second_d = L * (L - 1) * sum;
+        Vector3 e1 = ComputeBezierTangent(t);
+        second_d = second_d - Vector3.Dot(second_d, e1) * e1;
+        return second_d.normalized;
+    }
+
+    Vector3 Vec3Cross(Vector3 a, Vector3 b)
+    {
+        Vector3 v = Vector3.zero;
+
+        /////////////////////////////////////////////////////
+        /// #12                                           ///
+        /// compute cross product of two vectors a and b  ///
+        /////////////////////////////////////////////////////
+        v.x = a.y * b.z - a.z * b.y;
+        v.y = -1*(a.x * b.z - a.z * b.x);
+        v.z = a.x * b.y - a.y * b.x;
+
+
+        return v.normalized;
+    }
+
+    Vector3 ComputeFrenetNormal(Vector3 first_derivative, Vector3 second_derivative) {
+        Vector3 N = Vector3.zero;
+
+        /////////////////////////////////////////////////////
+        /// #13                                           ///
+        /// compute Normal Vector of Frenet frame         ///
+        /////////////////////////////////////////////////////
+        N = Vec3Cross(first_derivative, second_derivative);
+        return N.normalized;
+    }
+
+    public Quaternion RotToQuat(Matrix4x4 R)
+    {
+        Quaternion q = new Quaternion();
+
+        /////////////////////////////////////////////////////////////
+        /// #2                                                    ///
+        /// copy function implemented at #2 of ComputeRotation.cs ///
+        /////////////////////////////////////////////////////////////
+        q.w = Mathf.Sqrt(1 + R.m00 + R.m11 + R.m22) / 2;
+        q.x = (R.m21 - R.m12) / (4 * q.w);
+        q.y = (R.m02 - R.m20) / (4 * q.w);
+        q.z = (R.m10 - R.m01) / (4 * q.w);
+
+        return q;
+    }
+}
---
+++ RotationInterpolation.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class MatrixExtension
+{
+    public static Matrix4x4 Subtract(this Matrix4x4 mat, Matrix4x4 other)
+    {
+        // implement
+
+        return mat;
+    }
+}
+
+public class RotationInterpolation : MonoBehaviour
+{
+    Matrix4x4 EulerToRot(Vector3 v)
+    {
+        Matrix4x4 R = new Matrix4x4();
+        Matrix4x4 Rx = new Matrix4x4();
+        Matrix4x4 Ry = new Matrix4x4();
+        Matrix4x4 Rz = new Matrix4x4();
+
+        ///////////////////////////////////////////////////////////////////////
+        /// #1                                                              ///
+        /// Implement conversion from euler angles input to rotation matrix ///
+        /// !!!!!Unity3D engine uses YXZ intrinsic rotation!!!!!            ///
+        ///////////////////////////////////////////////////////////////////////
+
+
+        float xAng = v.x * Mathf.Deg2Rad;
+        float yAng = v.y * Mathf.Deg2Rad;
+        float zAng = v.z * Mathf.Deg2Rad;
+       
+        //Rx
+        Rx.m00 = Rx.m33 = 1;
+        Rx.m11 = Rx.m22 = Mathf.Cos(xAng);
+        Rx.m21 = Mathf.Sin(xAng);
+        Rx.m12 = -Rx.m21;
+        //Ry
+        Ry.m11 = Ry.m33 = 1;
+        Ry.m00 = Ry.m22 = Mathf.Cos(yAng);
+        Ry.m02 = Mathf.Sin(yAng);
+        Ry.m20 = -Ry.m02;
+        //Rz
+        Rz.m22 = Rz.m33 = 1;
+        Rz.m00 = Rz.m11 = Mathf.Cos(zAng);
+        Rz.m10 = Mathf.Sin(zAng);
+        Rz.m01 = -Rz.m10;
+
+        R = Ry * Rx * Rz;
+        return R;
+    }
+
+    Quaternion RotToQuat(Matrix4x4 R)
+    {
+        Quaternion q = new Quaternion();
+
+        ///////////////////////////////////////////////////////////////
+        /// #2                                                      ///
+        /// Implement conversion from rotation matrix to quaternion ///
+        ///////////////////////////////////////////////////////////////
+        q.w = Mathf.Sqrt(1 + R.m00 + R.m11 + R.m22) / 2;
+        q.x = (R.m21 - R.m12) / (4 * q.w);
+        q.y = (R.m02 - R.m20) / (4 * q.w);
+        q.z = (R.m10 - R.m01) / (4 * q.w);
+
+
+        return q;
+    }
+
+    Vector3 RotToExp(Matrix4x4 R)
+    {
+        float d= 0f;
+        Vector3 w_unit = Vector3.zero;
+
+        /////////////////////////////////////////////////////////////////////////////
+        /// #3                                                                    ///
+        /// Implement conversion from rotation matrix to angle-axis (exponential) ///
+        /////////////////////////////////////////////////////////////////////////////
+
+        float trace = R.m00 + R.m11 + R.m22;
+        d = Mathf.Acos((trace - 1) / 2);
+        Matrix4x4 Rt = R.transpose;
+        float val = (1 / (2 * Mathf.Sin(d)));
+        Matrix4x4 wHat = new Matrix4x4();
+
+        wHat.m00 = val * (R.m00 - Rt.m00);
+        wHat.m01 = val * (R.m01 - Rt.m01);
+        wHat.m02 = val * (R.m02 - Rt.m02);
+        wHat.m10 = val * (R.m10 - Rt.m10);
+        wHat.m11 = val * (R.m11 - Rt.m11);
+        wHat.m12 = val * (R.m12 - Rt.m12);
+        wHat.m20 = val * (R.m20 - Rt.m20);
+        wHat.m21 = val * (R.m21 - Rt.m21);
+        wHat.m22 = val * (R.m22 - Rt.m22);
+        wHat.m33 = 1;
+
+        w_unit.x = wHat.m21;
+        w_unit.y = wHat.m02;
+        w_unit.z = wHat.m10;
+
+        w_unit = w_unit.normalized;
+
+
+        return w_unit * d;
+    }
+
+    Quaternion ExpToQuat(Vector3 w)
+    {
+        Quaternion q = new Quaternion();
+
+        ///////////////////////////////////////////////////////////
+        /// #4                                                  ///
+        /// Implement conversion from exponential to quaternion ///
+        ///////////////////////////////////////////////////////////
+
+        float d = Mathf.Sqrt(w.x * w.x + w.y * w.y + w.z * w.z);
+        Vector3 w_unit = w / d;
+        q.w = Mathf.Cos(d / 2);
+        q.x = Mathf.Sin(d / 2) * w_unit.x;
+        q.y = Mathf.Sin(d / 2) * w_unit.y;
+        q.z = Mathf.Sin(d / 2) * w_unit.z;
+
+        return q;
+    }
+
+    public enum InterpolationMethod { EulerAngles, ExpCoord, Quat }
+    public InterpolationMethod interpmethod;
+
+    [Range(0.0f, 1.0f)]
+    public float interp; // interpolation parameter
+
+    // Input
+    public Vector3 start_EulerAngles; // initial orientation in Euler Angles
+    public Vector3 end_EulerAngles; // final orientation in Euler Angles
+
+    // Euler Angles interp
+    private Vector3 interp_v;
+    
+    // Rotation Matrix
+    private Matrix4x4 m_start, m_end; 
+
+    // Exponential Coordinate
+    private Vector3 w_start, w_end; 
+    private Vector3 interp_w;
+
+    //Quaternion
+    private Quaternion q_start, q_end;
+    private Quaternion interp_q;
+
+    // GUI text
+    public Text TextEuler;
+    public Text TextExpCoord;
+    public Text TextQuat;
+
+    // Use this for initialization
+    void Start()
+    {
+        TextEuler.text = "EulerAngles: (0.0, 0.0, 0.0)";
+        TextExpCoord.text = "Exp. Coords: (0.0, 0.0, 0.0)";
+        TextQuat.text = "Quaternion: (0.0, 0.0, 0.0, 1.0)";
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("HI");
+        //////////////////////////////////////////////////////////////////
+        /// #5                                                         ///
+        /// Implement linear interpolation of euler angles             ///
+        //////////////////////////////////////////////////////////////////
+
+        //////////////////////////////////////////////////////////////////
+        /// #6                                                         ///
+        /// Calculate interpolated rotations and apply it to the cube  ///
+        /// 1. Euler Angles (use rotation matrix)                      ///
+        /// 2. Exponential Coordinate                                  ///
+        /// 3. Quaternion                                              ///
+        ///                                                            ///
+        /// use transform.rotation = ~ /                               ///
+        /// use Quaternion.Slerp() for quaternion interpolation        ///
+        //////////////////////////////////////////////////////////////////
+
+        // apply
+        switch (interpmethod)
+        {
+            case InterpolationMethod.EulerAngles:
+                interp_v = new Vector3((1 - interp) * start_EulerAngles.x + interp * end_EulerAngles.x,
+                    (1 - interp) * start_EulerAngles.y + interp * end_EulerAngles.y,
+                    (1 - interp) * start_EulerAngles.z + interp * end_EulerAngles.z);
+                print(interp_v);
+                transform.rotation = RotToQuat(EulerToRot(interp_v));
+                break;
+
+            case InterpolationMethod.ExpCoord:
+                m_start = EulerToRot(start_EulerAngles);
+                m_end = EulerToRot(end_EulerAngles);
+                w_start = RotToExp(m_start);
+                w_end = RotToExp(m_end);
+                interp_w = new Vector3((1 - interp) * w_start.x + interp * w_end.x,
+                   (1 - interp) * w_start.y + interp * w_end.y,
+                   (1 - interp) * w_start.z + interp * w_end.z);
+                print(interp_w);
+                transform.rotation = ExpToQuat(interp_w);
+                break;
+
+            case InterpolationMethod.Quat:
+                
+                m_start = EulerToRot(start_EulerAngles);
+                m_end = EulerToRot(end_EulerAngles);
+                q_start = RotToQuat(m_start);
+                q_end = RotToQuat(m_end);
+                interp_q = Quaternion.Slerp(q_start, q_end, interp);
+                transform.rotation = interp_q;
+                break;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////
+        /// #7                                                                     ///
+        /// Visualize the rotation in euler angle, angle-axis (exp) and quaternion ///
+        /// ex) TextEuler.text = "EulerAngles: (" + value + ")";                   /// 
+        //////////////////////////////////////////////////////////////////////////////
+        TextEuler.text = "EulerAngles: (" + interp_v + ")";
+        TextExpCoord.text = "Exp. Coords: (" + interp_w + ")";
+        TextQuat.text = "Quaternion: ("+ interp_q + ")";
+    }
+}
